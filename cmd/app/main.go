1f9fd598c526a0eb593f9070fd3ec6882dadf18b
igorbelousov <belousov.ig@gmail.com>
2021-01-02 19:52:49 +0200

    add log

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("hello world")
+	log := log.New(os.Stdout, "APP: ", log.LstdFlags|log.Lmicroseconds|log.Lshortfile)
+
+	if err := run(log); err != nil {
+		log.Println("errror: ", err)
+		os.Exit(1)
+	}
+
+}
+
+func run(log *log.Logger) *error {
+	log.Println(log)
+	return nil
 }
