7dafa0d39ff0fee9b3eb4b4d30f0ab9e07920c3d
igorbelousov <belousov.ig@gmail.com>
2021-01-02 20:27:16 +0200

    add config

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,21 +1,76 @@
 package main
 
 import (
+	"expvar"
+	"fmt"
 	"log"
 	"os"
+	"time"
+
+	"github.com/ardanlabs/conf"
+	"github.com/pkg/errors"
 )
+
+var build = "develop"
 
 func main() {
 	log := log.New(os.Stdout, "APP: ", log.LstdFlags|log.Lmicroseconds|log.Lshortfile)
 
 	if err := run(log); err != nil {
-		log.Println("errror: ", err)
+		log.Println("App error: ", err)
 		os.Exit(1)
 	}
 
 }
 
-func run(log *log.Logger) *error {
-	log.Println(log)
+func run(log *log.Logger) error {
+	var cfg struct {
+		conf.Version
+		Web struct {
+			APIHost         string        `conf:"default:0.0.0.0:3000"`
+			DebugHost       string        `conf:"default:0.0.0.0:4000"`
+			ReadTimeout     time.Duration `conf:"default:5s"`
+			WriteTimeout    time.Duration `conf:"default:5s"`
+			ShutdownTimeout time.Duration `conf:"default:5s"`
+		}
+	}
+
+	cfg.Version.SVN = build
+	cfg.Version.Desc = "copyright information here"
+
+	if err := conf.Parse(os.Args[1:], "APP", &cfg); err != nil {
+		switch err {
+		case conf.ErrHelpWanted:
+			usage, err := conf.Usage("APP", &cfg)
+			if err != nil {
+				return errors.Wrap(err, "generating config usage")
+			}
+			fmt.Println(usage)
+			return nil
+		case conf.ErrVersionWanted:
+			version, err := conf.VersionString("APP", &cfg)
+			if err != nil {
+				return errors.Wrap(err, "generating config version")
+			}
+			fmt.Println(version)
+			return nil
+		}
+		return errors.Wrap(err, "parsing config")
+	}
+
+	// =========================================================================
+	// App Starting
+
+	// Print the build version for our logs. Also expose it under /debug/vars.
+	expvar.NewString("build").Set(build)
+	log.Printf("main : Started : Application initializing : version %q", build)
+	defer log.Println("main: Completed")
+
+	out, err := conf.String(&cfg)
+	if err != nil {
+		return errors.Wrap(err, "generating config for output")
+	}
+	log.Printf("main: Config :\n%v\n", out)
+
 	return nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/igorbelousov/go-web-core
 
 go 1.15
+
+require (
+	github.com/ardanlabs/conf v1.3.3
+	github.com/pkg/errors v0.9.1
+)
