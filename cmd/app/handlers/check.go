1df093d53feb4d5397e5441c9995a03234005798
igorbelousov <belousov.ig@gmail.com>
2021-01-04 20:25:40 +0200

    add respond

--- cmd/app/handlers/check.go
+++ cmd/app/handlers/check.go
@@ -2,9 +2,12 @@ package handlers
 
 import (
 	"context"
-	"encoding/json"
+	"errors"
 	"log"
+	"math/rand"
 	"net/http"
+
+	"github.com/igorbelousov/go-web-core/foundation/web"
 )
 
 type check struct {
@@ -13,12 +16,17 @@ type check struct {
 
 func (c check) readiness(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 
+	if n := rand.Intn(100); n%2 == 0 {
+		web.RespondError(ctx, w, err)
+		return errors.New("INt Error")
+	}
+
 	status := struct {
 		Status string
 	}{
 		Status: "OK",
 	}
 	log.Println(r, status)
-	return json.NewEncoder(w).Encode(status)
+	return web.Respond(ctx, w, status, http.StatusOK)
 
 }
