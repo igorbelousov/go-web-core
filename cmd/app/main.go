266c3ed0601b233ef74e3787a95d3da05b694478
igorbelousov <belousov.ig@gmail.com>
2021-01-09 17:51:28 +0200

    add docker file

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -46,7 +46,7 @@ func run(log *log.Logger) error {
 		}
 		Auth struct {
 			KeyID          string `conf:"default:54bb2165-71e1-41a6-af3e-7da4a0e1e2c1"`
-			PrivateKeyFile string `conf:"default:./private.pem"`
+			PrivateKeyFile string `conf:"default:/core/private.pem"`
 			Algorithm      string `conf:"default:RS256"`
 		}
 	}
--- makefile
+++ makefile
@@ -3,6 +3,16 @@ SHELL := /bin/bash
 export PROJECT = Go-Web-Core
 
 
+core:
+	docker build \
+	-f zarf/dockerfile.go-web-core \
+	-t go-web-core-amd64:1.0 \
+	--build-arg VSF_REF=`git rev-parse HEAD` \
+	--build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
+	.
+
+
+
 run:
 	go run ./cmd/app/main.go
 
