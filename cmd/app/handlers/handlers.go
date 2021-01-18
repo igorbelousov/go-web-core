e1ca76b463c48a95e63fd8c5843f5f3a306c8773
igorbelousov <belousov.ig@gmail.com>
2021-01-18 16:38:57 +0200

    add user handlers

--- cmd/app/handlers/handlers.go
+++ cmd/app/handlers/handlers.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/igorbelousov/go-web-core/foundation/web"
 	"github.com/igorbelousov/go-web-core/internal/auth"
+	"github.com/igorbelousov/go-web-core/internal/data/user"
 	"github.com/igorbelousov/go-web-core/internal/mid"
 	"github.com/jmoiron/sqlx"
 )
@@ -23,6 +24,17 @@ func API(build string, shutdown chan os.Signal, log *log.Logger, a *auth.Auth, d
 
 	app.Handle(http.MethodGet, "/readiness", cg.readiness)
 	app.Handle(http.MethodGet, "/liveiness", cg.liveness)
+
+	ug := userGroup{
+		user: user.New(log, db),
+		auth: a,
+	}
+	app.Handle(http.MethodGet, "/v1/users/:page/:rows", ug.query, mid.Authenticate(a), mid.Authorize(auth.RoleAdmin))
+	app.Handle(http.MethodGet, "/v1/users/token/:kid", ug.token)
+	app.Handle(http.MethodGet, "/v1/users/:id", ug.queryByID, mid.Authenticate(a))
+	app.Handle(http.MethodPost, "/v1/users", ug.create, mid.Authenticate(a), mid.Authorize(auth.RoleAdmin))
+	app.Handle(http.MethodPut, "/v1/users/:id", ug.update, mid.Authenticate(a), mid.Authorize(auth.RoleAdmin))
+	app.Handle(http.MethodDelete, "/v1/users/:id", ug.delete, mid.Authenticate(a), mid.Authorize(auth.RoleAdmin))
 
 	return app
 }
