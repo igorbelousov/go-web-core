374315d5d387afece2387b74e9dc9b8d5149657e
igorbelousov <belousov.ig@gmail.com>
2021-01-19 19:01:21 +0200

    update

--- internal/data/schema/schema.go
+++ internal/data/schema/schema.go
@@ -31,42 +31,4 @@ CREATE TABLE users (
 	PRIMARY KEY (user_id)
 );`,
 	},
-	{
-		Version:     1.2,
-		Description: "Create table products",
-		Script: `
-CREATE TABLE products (
-	product_id   UUID,
-	name         TEXT,
-	cost         INT,
-	quantity     INT,
-	date_created TIMESTAMP,
-	date_updated TIMESTAMP,
-
-	PRIMARY KEY (product_id)
-);`,
-	},
-	{
-		Version:     1.3,
-		Description: "Create table sales",
-		Script: `
-CREATE TABLE sales (
-	sale_id      UUID,
-	product_id   UUID,
-	quantity     INT,
-	paid         INT,
-	date_created TIMESTAMP,
-
-	PRIMARY KEY (sale_id),
-	FOREIGN KEY (product_id) REFERENCES products(product_id) ON DELETE CASCADE
-);`,
-	},
-	{
-		Version:     2.1,
-		Description: "Alter table products with user column",
-		Script: `
-ALTER TABLE products
-	ADD COLUMN user_id UUID DEFAULT '00000000-0000-0000-0000-000000000000'
-`,
-	},
 }
